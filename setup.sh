00dc67670407b44c9413b5ed43ad801c0a50c504
coutinhorony <81978949+coutinhorony@users.noreply.github.com>
2025-11-12 11:55:33 -0300

    Randomize region selection for resource creation

--- setup.sh
+++ setup.sh
@@ -8,13 +8,14 @@ suffix=${suffix:0:18}
 # Set the necessary variables
 RESOURCE_GROUP="rg-dp100-l${suffix}"
 RESOURCE_PROVIDER="Microsoft.MachineLearningServices"
-REGION="eastus"
+REGIONS=("eastus" "westus" "centralus" "northeurope" "westeurope")
+RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
 WORKSPACE_NAME="mlw-dp100-l${suffix}"
 COMPUTE_INSTANCE="ci${suffix}"
 COMPUTE_CLUSTER="aml-cluster"
-storageAccountName="sampleblobstorage${suffix}"
-containerName="sampleblobdp100"
-containerName2="secondcontainer"
+storageAccountName="blobstorage${suffix}"
+containerName="blobstorage"
+containerName2="blobstorage2"
 
 # Register the Azure Machine Learning resource provider in the subscription
 echo "Register the Machine Learning resource provider:"
@@ -22,16 +23,24 @@ az provider register --namespace $RESOURCE_PROVIDER
 
 # Create the resource group and workspace and set to default
 echo "Create a resource group and set as default:"
-az group create --name $RESOURCE_GROUP --location $REGION
+az group create --name $RESOURCE_GROUP --location $RANDOM_REGION
 az configure --defaults group=$RESOURCE_GROUP
 
 echo "Create an Azure Machine Learning workspace:"
 az ml workspace create --name $WORKSPACE_NAME 
 az configure --defaults workspace=$WORKSPACE_NAME 
 
+# Create compute instance
+echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
+az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS11_V2 --type ComputeInstance 
+
+# Create compute cluster
+echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
+
 echo "Create an Azure Machine Learning BlobStorage:"
 # Crie uma conta de armazenamento
-az storage account create --name $storageAccountName --resource-group $RESOURCE_GROUP --location $REGION --sku Standard_LRS
+az storage account create --name $storageAccountName --resource-group $RESOURCE_GROUP --location $RANDOM_REGION --sku Standard_LRS
 
 echo "Create an Azure Machine Learning Container:"
 # Crie um container de blob
@@ -40,12 +49,4 @@ az storage container create --name $containerName --account-name $storageAccount
 echo "Create an Azure Machine Learning Containeir2:"
 # Crie um container de blob 2 
 az storage container create --name $containerName2 --account-name $storageAccountName
-
-# Create compute instance
-echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
-az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_D2A_V4 --type ComputeInstance 
-
-# Create compute cluster
-echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_D2A_V4 --max-instances 2 --type AmlCompute 
 
